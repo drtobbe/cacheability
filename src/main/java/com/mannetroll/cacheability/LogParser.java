2c621086f43c1c26337f4b656911c1687e09eb63
drtobbe <drtobbe@me.com>
2014-10-19 12:02:21 +0200

    LogParser

--- src/main/java/com/mannetroll/cacheability/LogParser.java
+++ src/main/java/com/mannetroll/cacheability/LogParser.java
@@ -1,8 +1,12 @@
 package com.mannetroll.cacheability;
 
+import java.io.BufferedReader;
 import java.io.File;
+import java.io.FileReader;
+import java.io.IOException;
 import java.text.ParseException;
 import java.text.SimpleDateFormat;
+import java.util.regex.Matcher;
 import java.util.regex.Pattern;
 
 import org.slf4j.Logger;
@@ -10,7 +14,13 @@ import org.slf4j.LoggerFactory;
 
 public class LogParser {
     private final static Logger logger = LoggerFactory.getLogger(LogParser.class);
-    Pattern accessLogPattern = Pattern.compile(getAccessLogRegex(), Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+
+    /*Log file feilds*/
+    String clientHost = null;
+    String requestTime = null;
+    String clientRequest = null;
+    String httpStatusCode = null;
+    String numOfBytes = null;
 
     public LogParser() {
     }
@@ -45,6 +55,32 @@ public class LogParser {
 
     public void parse(File file) {
         logger.info("file: " + file);
+        try {
+            BufferedReader bufferReader = new BufferedReader(new FileReader(file));
+            String line = "";
+            long index = 0;
+
+            Pattern accessLogPattern = Pattern.compile(getAccessLogRegex(), Pattern.CASE_INSENSITIVE | Pattern.DOTALL);
+            Matcher accessLogEntryMatcher;
+            while ((line = bufferReader.readLine()) != null) {
+                index++;
+                accessLogEntryMatcher = accessLogPattern.matcher(line);
+                if (!accessLogEntryMatcher.matches()) {
+                    System.out.println("" + index + " : couldn't be parsed");
+                    continue;
+                } else {
+                    clientRequest = accessLogEntryMatcher.group(5);
+                    String verb = clientRequest.split(" ")[0];
+                    String url = clientRequest.split(" ")[1];
+                    System.out.println("" + index + " : " + url);
+                }
+            }
+            bufferReader.close();
+        } catch (IOException e) {
+            logger.error(e.getMessage(), e);
+        } finally {
+
+        }
 
     }
 
