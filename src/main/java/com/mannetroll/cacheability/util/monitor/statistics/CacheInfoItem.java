415dbc971ed31d78f3e03f5cc35938c54a3beded
drtobbe <drtobbe@me.com>
2014-10-19 14:09:16 +0200

    cachehit

--- src/main/java/com/mannetroll/cacheability/util/monitor/statistics/CacheInfoItem.java
+++ src/main/java/com/mannetroll/cacheability/util/monitor/statistics/CacheInfoItem.java
@@ -3,6 +3,9 @@ package com.mannetroll.cacheability.util.monitor.statistics;
 public class CacheInfoItem {
     private final String key;
     private long lastcall;
+    private long TTL = 300 * 1000;
+    private int cachehit = 0;
+    private int cachemiss = 0;
 
     public CacheInfoItem(String key, long now) {
         this.key = key;
@@ -11,7 +14,14 @@ public class CacheInfoItem {
 
     public void addCacheCall(double timeSlice, int chunk, long now) {
         long age = now - lastcall;
-        System.out.println("age: " + age + " - " + key);
+        if (age < TTL) {
+            cachehit++;
+            System.out.println("cachehit: " + cachehit + " - " + key);
+        } else {
+            cachemiss++;
+            System.out.println("cachemiss: " + cachemiss + " - " + key);
+            lastcall = now;
+        }
     }
 
 }
--- src/test/java/com/mannetroll/cacheability/LogParserTest.java
+++ src/test/java/com/mannetroll/cacheability/LogParserTest.java
@@ -9,8 +9,8 @@ public class LogParserTest {
     @Test
     public void test() {
         LogParser logParser = new LogParser();
-        File file = new File("src/test/resources/test.log");
-        //File file = new File("/Users/drtobbe/Desktop/cacheability/access_findNearestByAddress.log");
+        //File file = new File("src/test/resources/test.log");
+        File file = new File("/Users/drtobbe/Desktop/cacheability/access_findNearestByAddress.log");
         logParser.parse(file);
     }
 
